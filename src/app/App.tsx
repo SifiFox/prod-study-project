c4d65fcad6dae4bea206a447814e971c67e1101a
SifiFox <dima.antonovich.1999@gmail.com>
2023-05-26 17:08:37 +0300

    feat: add btn; feat: add navbar;

---
+++ src/app/App.tsx
@@ -0,0 +1,19 @@
+import { Link } from "react-router-dom";
+import "./styles/index.scss";
+import { classNames } from "shared/lib/classNames/classNames";
+import { useTheme } from "./providers/ThemeProvider";
+import { AppRouter } from "./providers/router";
+import { Navbar } from "widgets/Navbar";
+
+const App = () => {
+  const { theme } = useTheme();
+
+  return (
+    <div className={classNames("app", {}, [theme])}>
+      <Navbar />
+      <AppRouter />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/shared/ui/AppLink/AppLink.tsx
@@ -0,0 +1,28 @@
+import { Link, LinkProps } from "react-router-dom";
+import cls from "./AppLink.module.scss";
+import { FC } from "react";
+import { classNames } from "shared/lib/classNames/classNames";
+
+export enum AppLinkTheme {
+  PRIMARY = "primary",
+  SECONDARY = "secondary",
+}
+
+interface AppLinkProps extends LinkProps {
+  className?: string;
+  theme?: AppLinkTheme;
+}
+
+export const AppLink: FC<AppLinkProps> = (props) => {
+  const { to, children, className, theme = "primary", ...otherProps } = props;
+
+  return (
+    <Link
+      to={to}
+      className={classNames(cls.AppLink, {}, [className, cls[theme]])}
+      {...otherProps}
+    >
+      {children}
+    </Link>
+  );
+};
